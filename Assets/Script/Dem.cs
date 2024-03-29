0eed67e72d8b687b972cc19fdb19d550515d9ca9
siweiqi <siweiqi@ivymobile.com>
2024-03-29 10:34:32 +0800

    fix

--- Assets/Script/Dem.cs
+++ Assets/Script/Dem.cs
@@ -10,43 +10,43 @@ public class Dem : MonoBehaviour
 
     void Awake()
     {
-        RiseSdk.RiseSdk.Instance.OnInit();
-        // #if UNITY_ANDROID
-        RiseSdk.RiseSdkListener.OnGMSEvent += (eventType, str) =>
-        {
-            RiseSdk.RiseSdk.Instance.ToastMsg($"{eventType.ToString()} + {str}");
-        };
+        // RiseSdk.RiseSdk.Instance.OnInit();
+        // // #if UNITY_ANDROID
+        // RiseSdk.RiseSdkListener.OnGMSEvent += (eventType, str) =>
+        // {
+        //     RiseSdk.RiseSdk.Instance.ToastMsg($"{eventType.ToString()} + {str}");
+        // };
 
-        RiseSdk.RiseSdkListener.OnAdEvent += (eventType, id, str, h) =>
-        {
-            RiseSdk.RiseSdk.Instance.ToastMsg($"{eventType.ToString()} + {id} + {str} + {h}");
-        };
+        // RiseSdk.RiseSdkListener.OnAdEvent += (eventType, id, str, h) =>
+        // {
+        //     RiseSdk.RiseSdk.Instance.ToastMsg($"{eventType.ToString()} + {id} + {str} + {h}");
+        // };
 
-        // #endif
+        // // #endif
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.Find("rewarded").GetComponent<Button>().onClick.AddListener(() =>
-        {
+        // transform.Find("rewarded").GetComponent<Button>().onClick.AddListener(() =>
+        // {
 
-            Debug.Log("call show Reward Ad");
-            RiseSdk.RiseSdk.Instance.ToastMsg("call show Reward Ad");
-            // #if UNITY_ANDROID
-            if (RiseSdk.RiseSdk.Instance.HasRewardAd())
-            {
-                RiseSdk.RiseSdk.Instance.ShowRewardAd(1);
-            }
-            else
-            {
-                RiseSdk.RiseSdk.Instance.ToastMsg("No Reward Ad");
-            }
-            // #elif UNITY_EDITOR
-            //     RiseSdk.RiseSdk.Instance.ShowRewardAd(1);
-            // #endif
-            //  RiseSdk.Instance.callNoMethod();
-        });
+        //     Debug.Log("call show Reward Ad");
+        //     RiseSdk.RiseSdk.Instance.ToastMsg("call show Reward Ad");
+        //     // #if UNITY_ANDROID
+        //     if (RiseSdk.RiseSdk.Instance.HasRewardAd())
+        //     {
+        //         RiseSdk.RiseSdk.Instance.ShowRewardAd(1);
+        //     }
+        //     else
+        //     {
+        //         RiseSdk.RiseSdk.Instance.ToastMsg("No Reward Ad");
+        //     }
+        //     // #elif UNITY_EDITOR
+        //     //     RiseSdk.RiseSdk.Instance.ShowRewardAd(1);
+        //     // #endif
+        //     //  RiseSdk.Instance.callNoMethod();
+        // });
     }
 
     // Update is called once per frame
